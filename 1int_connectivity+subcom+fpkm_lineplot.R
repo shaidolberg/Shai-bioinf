0db87125663c016d9a62aa48fbea63e83805de4f
shaidolberg <shaidolberg@gmail.com>
2018-06-07 15:33:04 +0300

    fpkm added

--- 1int_connectivity+subcom+fpkm_lineplot.R
+++ 1int_connectivity+subcom+fpkm_lineplot.R
@@ -19,7 +19,7 @@ table_down <- args[2]
 time.start=proc.time()[[3]]
 
 #downstream##########################################
-#table_down <- "D:/shai/hiC_chip-seq/intron_data/CTCF_intron_chr19.table_down.txt"
+table_down <- "D:/shai/hiC_chip-seq/intron_data/CTCF_intron_chr19.table_down.txt"
 table1<-read.table(table_down, sep = "\t", header = FALSE)
 #table1 <- table1[1:20,]
 vars <- colsplit(table1$V4, "_", c("ENST", "subcomp" ,"FPKM","connctivity"))
@@ -31,7 +31,7 @@ table1 <- table1[order(table1$ENST),]#order by ENST
 bigwig_d <- table1[,11:ncol(table1)]
 end_loci <- length(bigwig_d)
 #upstream#################################################
-#table_up <- "D:/shai/hiC_chip-seq/intron_data/CTCF_intron_chr19.table_up.txt"
+table_up <- "D:/shai/hiC_chip-seq/intron_data/CTCF_intron_chr19.table_up.txt"
 table1u<-read.table(table_up,sep = "\t")
 #table1u <- table1u[1:20,]
 varsu <- colsplit(table1u$V4, "_", c("ENST", "subcomp" ,"FPKM","connctivity"))
@@ -149,9 +149,9 @@ ggtitle(paste0("p=", p_v )) +
 theme(legend.position="none") +
 coord_cartesian(ylim = ylim1*1.8)+ # scale y limits based on ylim1
 theme(axis.title.x=element_blank(), axis.title.y=element_blank(), plot.title = element_text(size = rel(1)))
-
-###########################################################
 '
+###########################################################
+
 mean_bin_table$Connectivity <- c("Low","Medium","High","Highest")
 
 m <- as.data.frame(melt(mean_bin_table,id.vars="Connectivity"))#melting the table so each loci is infront of its bin
@@ -182,8 +182,76 @@ print(p)
 #print(grid.arrange(p, box ,nrow=1, ncol=2,newpage = TRUE, widths=c(3,1)))
 dev.off()
 
-#### fpkm
-#aojho
+#### fpkm ###################################################################
+FPKMtb <- full_table[order(full_table$FPKM),] # sorting from the smallest to the biggest
+binVector <- data.frame(matrix(NA, nrow = length(FPKMtb$FPKM), ncol = 1))
+FPKMtb <- append(FPKMtb,binVector,1)
+FPKMtb <- as.data.frame(FPKMtb)
+colnames(FPKMtb)[2] <- "bin"
+bin_0 <- which(FPKMtb$FPKM==0)
+FPKMtb$bin[bin_0] <- 0 #setting the 0 bin
+
+x <- (length(FPKMtb$FPKM)-length(bin_0))%%bins
+if(x!=0){
+  FPKMtb <-  FPKMtb[-c(1:x),] 
+}
+
+part <- ((length(FPKMtb$FPKM)-length(bin_0))/bins)#getting the size of the bins
+# giving a bin number to the designated part of the table, besides the zerom bin 
+for (i in 1:bins){
+  for (line in (1+(i-1)*part):((i*part))) {
+    FPKMtb$bin[length(bin_0)+line] <- i
+  }
+}
+
+mean_bin_table_fpkm <- data.frame(matrix(NA, nrow = 0:bins, ncol = length(position)))
+for (y in 0:3){   
+  #n = as.character(length(which(uni_tb$bin==(y))))
+  #write(paste0(y," n=",n),"/home/shaidulberg/chipseq/Modifications/exons_n.txt",append = TRUE)
+  mean_bin_vector_fpkm <-  apply(FPKMtb[which(FPKMtb$bin==(y)),12:ncol(FPKMtb)], 2, mean , na.rm = TRUE)
+  mean_bin_vector_fpkm <-  append(mean_bin_vector_fpkm,y,0) 
+  mean_bin_table_fpkm <- rbind(mean_bin_table_fpkm,mean_bin_vector_fpkm)
+  colnames(mean_bin_table_fpkm) <-c("bin",position)
+}
+mean_bin_table_fpkm$bin <- c("Non","Low","Medium","High")
+
+qtr1 <- FPKMtb[which(FPKMtb$bin ==  0),]
+qtr4 <- FPKMtb[which(FPKMtb$bin ==  4),]
+mean_qtr1 <- t(as.matrix(apply(qtr1[, 12:ncol(qtr1)], 2, mean, na.rm=TRUE)))
+mean_qtr4 <- t(as.matrix(apply(qtr4[, 12:ncol(qtr4)], 2, mean, na.rm=TRUE)))
+max_mean_qtr1 <- (apply(qtr1[,which.max(mean_qtr1)-500:which.max(mean_qtr1)+500], 1, mean, na.rm=TRUE))
+max_mean_qtr4 <- (apply(qtr4[,which.max(mean_qtr4)-500:which.max(mean_qtr4)+500], 1, mean, na.rm=TRUE))
+result <- t.test(x = max_mean_qtr1 ,y = max_mean_qtr4)
+p_v <- as.character(scientific(result$p.value, digits = 3))
+t_v <- as.character(scientific(result$statistic , digits = 3))
+
+out <- paste0("t.v:",t_v ,", p.v:",p_v)
+
+m <- melt(mean_bin_table_fpkm,id.vars="bin")#melting the table so each loci is infront of its bin
+colnames(m) <-c("FPKM","Loci","ChIP_seq_Signal")
+m$FPKM <- factor(m$FPKM, levels= c("High","Medium","Low","Non"), labels=c("High","Medium","Low","Non"))
+m$Loci <-as.numeric(as.character(m$Loci))
+m$Mean_FPKM <- as.numeric(m$Mean_FPKM)
+
+p <- ggplot(m, aes(x = Loci, y = ChIP_seq_Signal, group = FPKM)) +
+  geom_line(aes(color = FPKM), size = 0.7) +
+  scale_x_continuous(breaks = c(-5000,-75,5000) , labels = c(-5000,"TSS",5000)) +
+  #coord_cartesian(ylim = c(0, 25)) +
+  geom_segment(aes(x=-75,xend=75,y=0,yend=0),lwd=4,color="black")+
+  labs(y=paste0("ChIP-seq Signal"))+
+  geom_segment(aes(x=-start_loci,xend=end_loci,y=0,yend=0),lwd=1,color="black")+
+  scale_colour_manual(name='FPKM', values=c("Non"='black', "Low"='saddlebrown', "Medium" = 'red3', "High" = "red1"), guide='legend') +
+  #theme(legend.position="none")+ #no legend
+  #theme(legend.justification = c("right", "top"),legend.position = c(.95, .95))+ #option for legend on the figure
+  ggtitle(name, subtitle = out)
+
+setwd("/home/shaidulberg/chipseq/Modifications/1intron_connectivity_figuers")
+#setwd("D:/shai/hiC_chip-seq/intron_data")
+png(file= paste0(name,"_1st_intron.png"),width=850,height=600,res = 150)
+print(p)
+#print(grid.arrange(p, box ,nrow=1, ncol=2,newpage = TRUE, widths=c(3,1)))
+dev.off()
+
 stopCluster(cl)
 time.end=proc.time()[[3]]
 time.end-time.start
